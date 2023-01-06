6a289292e2633349056a0eddc12549b5ab7eef42
Jean-KOUAGOU <jeank@aims.ac.za>
2023-01-06 13:11:47 +0100

    clean up

--- generators/kb_to_kg.py
+++ generators/kb_to_kg.py
@@ -6,5 +6,5 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--kbs', type=str, nargs='+', default=['carcinogenesis'], help='Knowledge base name')
 
 for kb in parser.parse_args().kbs:
-    triples = RDFTriples(source_kg_path=f'../datasets/{kb}/{kb}.owl')
+    triples = RDFTriples(source_kb_path=f'../datasets/{kb}/{kb}.owl')
     triples.export_triples()
